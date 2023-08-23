71bd5f591a349dad6debad80325bc81bd6f5bed9
vasyaqwe <vasyaqwe>
2023-08-23 11:26:41 +0300

    refactor PersistLogin refresh call

--- client/src/wrappers/PersistLogin.tsx
+++ client/src/wrappers/PersistLogin.tsx
@@ -1,34 +1,22 @@
-import { useQuery, useQueryClient } from "react-query"
+import { useQuery } from "react-query"
 import { refresh } from "@/api/auth"
 import { useAuthStore } from "@/stores/useAuthStore"
 import { Outlet } from "react-router-dom"
 import { Spinner } from "@/components/ui/Spinner"
-import { useEffect, useRef } from "react"
 import { pageSpinnerClassName } from "@/utils"
 
 export const PersistLogin = () => {
-    const queryClient = useQueryClient()
     const { setToken, token, persist } = useAuthStore()
-
-    const effectRan = useRef(false)
 
     const { isLoading } = useQuery(["auth"], () => refresh(), {
         onSuccess: (res) => {
-            queryClient.invalidateQueries(["auth"])
             if (!token && persist) {
                 setToken(res.accessToken)
-                effectRan.current = true
             }
         },
         retry: false,
-        enabled: !effectRan.current,
+        refetchOnWindowFocus: false,
     })
-
-    useEffect(() => {
-        if (!effectRan.current && !isLoading) {
-            effectRan.current = true
-        }
-    }, [isLoading])
 
     return (
         <>
